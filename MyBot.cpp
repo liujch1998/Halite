a73d4b9ebd714e41e14f6e0ca768802e0877c80b
Liu Jiacheng <liujch1998@gmail.com>
2016-12-03 14:45:45 -0600

    NULL

--- MyBot.cpp
+++ MyBot.cpp
@@ -25,17 +25,19 @@ int main ()
     sendInit("Since 08/17/1926");
 
 	std::set<hlt::Location> boarders;
-    while(true)
+    int cycleCounter = 0;
+	while(true)
 	{
         moves.clear();
         getFrame(presentMap);
 
 		boarders.clear ();
+		int myTerritoryArea = 0;
 		for (unsigned short a = 0; a < presentMap.height; a ++)
 		{
 			for (unsigned short b = 0; b < presentMap.width; b ++)
 			{
-				if (presentMap.getSite ({b, a}, STILL).owner == myID) continue;
+				if (presentMap.getSite ({b, a}, STILL).owner == myID) { myTerritoryArea ++; continue; }
 				bool isBoarder = 0;
 				for (unsigned char i = 1; i < 5; i ++)
 					if (presentMap.getSite ({b, a}, i).owner == myID)
@@ -54,16 +56,15 @@ int main ()
 					for (std::set<hlt::Location>::iterator it = boarders.begin (); it != boarders.end (); it ++)
 					{
 						float  dist = presentMap.getDistance ({b, a}, *it);
-					//	log << "dist is " << dist << std::endl;
 						float angle = presentMap.getAngle    ({b, a}, *it);
-					//	float force = /*(float) presentMap.getSite (*it, STILL).production*/ 1.0 /*/ (float) presentMap.getSite (*it, STILL).production*/ / (dist * dist);
-					//	float force = /*(float) presentMap.getSite (*it, STILL).production*/ 1.0 / (float) presentMap.getSite (*it, STILL).production / (dist * dist);
-						float force = (float) presentMap.getSite (*it, STILL).production /* (float) presentMap.getSite (*it, STILL).production*/ / (dist * dist * dist);
+						float force;
+						if (myTerritoryArea >= 8)
+							force = (float) presentMap.getSite (*it, STILL).production / (dist * dist);
+						else force = (1.0f / (float) presentMap.getSite (*it, STILL).strength) / (dist * dist);
 						force_x += force * cos (angle);
 						force_y += force * sin (angle);
 					}
-				//	log << force_x << ' ' << force_y << std::endl;
-				//	if (presentMap.getSite ({b, a}, STILL).strength < 16) continue;
+					if (presentMap.getSite ({b, a}, STILL).strength < 16) continue;
 					unsigned char direction = STILL;
 					if (force_x + force_y > 0 && force_x - force_y > 0) direction = EAST;
 					if (force_x + force_y > 0 && force_x - force_y < 0) direction = SOUTH;
@@ -75,6 +76,7 @@ int main ()
 				}
 			}
 		}
+		cycleCounter ++;
 
         sendFrame(moves);
     }
