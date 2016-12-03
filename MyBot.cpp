e4b29e932557d17dd3e948a49886bedc0ce8611a
Liu Jiacheng <liujch1998@gmail.com>
2016-12-03 16:55:58 -0600

    v12

--- MyBot.cpp
+++ MyBot.cpp
@@ -6,80 +6,103 @@
 #include <set>
 #include <fstream>
 #include <cmath>
+#include <map>
 
 #include "hlt.hpp"
 #include "networking.hpp"
 
+float compute_force (hlt::Site site, float dist, int myArea)
+{
+	float force;
+	float production = (float) site.production;
+	float strength = (float) site.strength;
+	if (myArea > 16)
+		force = production / (dist * dist);
+	else
+		force = (1.0f / strength) / (dist * dist);
+	return force;
+}
+
 int main ()
 {
-	std::ofstream log;
-	log.open ("log.txt");
+	std::ofstream fout;	fout.open ("fout.log");
+	srand (time (NULL));
+	std::cout.sync_with_stdio (0);
 
-    srand(time(NULL));
-    std::cout.sync_with_stdio(0);
-    std::set<hlt::Move> moves; // output: moves
+	unsigned char myID;
+	hlt::GameMap currMap;
+	std::set<hlt::Move> moveList;
+	getInit (myID, currMap);
+	sendInit ("Since 08/17/1926");
 
-    unsigned char myID;
-    hlt::GameMap presentMap; // input: presentMap
-    getInit(myID, presentMap);
-    sendInit("Since 08/17/1926");
-
-	std::set<hlt::Location> boarders;
-    int cycleCounter = 0;
-	while(true)
+	int clock;
+	for (clock = 0; ; clock ++)
 	{
-        moves.clear();
-        getFrame(presentMap);
+		moveList.clear ();
+		getFrame (currMap);
 
-		boarders.clear ();
-		int myTerritoryArea = 0;
-		for (unsigned short a = 0; a < presentMap.height; a ++)
+		std::set<hlt::Location> boarders;
+		std::map<hlt::Move, bool> moveMap;
+		int myArea = 0;
+		for (unsigned short a = 0; a < currMap.height; a ++)
 		{
-			for (unsigned short b = 0; b < presentMap.width; b ++)
+			for (unsigned short b = 0; b < currMap.width; b ++)
 			{
-				if (presentMap.getSite ({b, a}, STILL).owner == myID) { myTerritoryArea ++; continue; }
-				bool isBoarder = 0;
-				for (unsigned char i = 1; i < 5; i ++)
-					if (presentMap.getSite ({b, a}, i).owner == myID)
-					{ isBoarder = 1; break; }
-				if (isBoarder)
-					boarders.insert ({b, a});
+				if (currMap.getSite ({b, a}, STILL).owner == myID)
+					myArea ++;
+				else
+				{
+					for (unsigned char i = 1; i < 5; i ++)
+						if (currMap.getSite ({b, a}, i).owner == myID)
+						{
+							boarders.insert ({b, a});
+							break;
+						}
+				}
 			}
 		}
-		for (unsigned short a = 0; a < presentMap.height; a++)
+		for (unsigned short a = 0; a < currMap.height; a++)
 		{
-			for (unsigned short b = 0; b < presentMap.width; b++)
+			for (unsigned short b = 0; b < currMap.width; b++)
 			{
-				if (presentMap.getSite ({b, a}).owner == myID)
+				if (currMap.getSite ({b, a}).owner == myID)
 				{
 					float force_x = 0.0, force_y = 0.0;
 					for (std::set<hlt::Location>::iterator it = boarders.begin (); it != boarders.end (); it ++)
 					{
-						float  dist = presentMap.getDistance ({b, a}, *it);
-						float angle = presentMap.getAngle    ({b, a}, *it);
-						float force;
-						if (myTerritoryArea >= 8)
-							force = (float) presentMap.getSite (*it, STILL).production / (dist * dist);
-						else force = (1.0f / (float) presentMap.getSite (*it, STILL).strength) / (dist * dist);
+						float  dist = currMap.getDistance ({b, a}, *it);
+						float angle = currMap.getAngle    ({b, a}, *it);
+						float force = compute_force (currMap.getSite (*it, STILL), dist, myArea);
 						force_x += force * cos (angle);
 						force_y += force * sin (angle);
 					}
-					if (presentMap.getSite ({b, a}, STILL).strength < 16) continue;
 					unsigned char direction = STILL;
 					if (force_x + force_y > 0 && force_x - force_y > 0) direction = EAST;
 					if (force_x + force_y > 0 && force_x - force_y < 0) direction = SOUTH;
 					if (force_x + force_y < 0 && force_x - force_y < 0) direction = WEST;
 					if (force_x + force_y < 0 && force_x - force_y > 0) direction = NORTH;
-					if (presentMap.getSite ({b, a}, direction).owner != myID && presentMap.getSite ({b, a}, direction).strength >= presentMap.getSite ({b, a}).strength) continue;
-
-					moves.insert ({{b, a}, direction});
+					hlt::Site currSite = currMap.getSite ({b, a}, STILL);
+					hlt::Site nextSite = currMap.getSite ({b, a}, direction);
+					if (myArea <= 16 && currSite.strength <= 8 || myArea > 16 && currSite.strength <= 16) continue; // let small pieces be resting ans growing
+					if (nextSite.owner != myID && currSite.strength <= nextSite.strength)
+					{
+						// change direction to merge and attack
+							 if (currMap.getSite ({b, a}, (direction + 0) & 0x3 + 1).owner == myID) direction = (direction + 0) & 0x3 + 1;
+						else if (currMap.getSite ({b, a}, (direction + 2) & 0x3 + 1).owner == myID) direction = (direction + 2) & 0x3 + 1;
+						else continue; // avoid death move
+					}
+					hlt::Move revMove = {currMap.getLocation ({b, a}, direction), (unsigned char) ((direction + 1) & 0x3 + 1)};
+					if (myArea <= 16 && moveMap[revMove] == 1) continue;
+					hlt::Move move = {{b, a}, direction};
+					moveList.insert (move);
+					moveMap[move] = 1;
 				}
 			}
 		}
-		cycleCounter ++;
 
-        sendFrame(moves);
-    }
+		sendFrame (moveList);
+	}
 
-    return 0;
+	return 0;
 }
+
--- RandomBot.cpp
+++ RandomBot.cpp
@@ -10,74 +10,93 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 
+float compute_force (hlt::Site site, float dist, int myArea)
+{
+	float force;
+	float production = (float) site.production;
+	float strength = (float) site.strength;
+	if (myArea > 16)
+		force = production / (dist * dist);
+	else
+		force = (1.0f / strength) / (dist * dist);
+	return force;
+}
+
 int main ()
 {
-	std::ofstream log;
-	log.open ("log.txt");
+	std::ofstream fout;	fout.open ("fout.log");
+	srand (time (NULL));
+	std::cout.sync_with_stdio (0);
 
-    srand(time(NULL));
-    std::cout.sync_with_stdio(0);
-    std::set<hlt::Move> moves; // output: moves
+	unsigned char myID;
+	hlt::GameMap currMap;
+	std::set<hlt::Move> moveList;
+	getInit (myID, currMap);
+	sendInit ("Since 08/17/1926");
 
-    unsigned char myID;
-    hlt::GameMap presentMap; // input: presentMap
-    getInit(myID, presentMap);
-    sendInit("Since 08/17/1926");
-
-	std::set<hlt::Location> boarders;
-    while(true)
+	int clock;
+	for (clock = 0; ; clock ++)
 	{
-        moves.clear();
-        getFrame(presentMap);
+		moveList.clear ();
+		getFrame (currMap);
 
-		boarders.clear ();
-		for (unsigned short a = 0; a < presentMap.height; a ++)
+		std::set<hlt::Location> boarders;
+		int myArea = 0;
+		for (unsigned short a = 0; a < currMap.height; a ++)
 		{
-			for (unsigned short b = 0; b < presentMap.width; b ++)
+			for (unsigned short b = 0; b < currMap.width; b ++)
 			{
-				if (presentMap.getSite ({b, a}, STILL).owner == myID) continue;
-				bool isBoarder = 0;
-				for (unsigned char i = 1; i < 5; i ++)
-					if (presentMap.getSite ({b, a}, i).owner == myID)
-					{ isBoarder = 1; break; }
-				if (isBoarder)
-					boarders.insert ({b, a});
+				if (currMap.getSite ({b, a}, STILL).owner == myID)
+					myArea ++;
+				else
+				{
+					for (unsigned char i = 1; i < 5; i ++)
+						if (currMap.getSite ({b, a}, i).owner == myID)
+						{
+							boarders.insert ({b, a});
+							break;
+						}
+				}
 			}
 		}
-		for (unsigned short a = 0; a < presentMap.height; a++)
+		for (unsigned short a = 0; a < currMap.height; a++)
 		{
-			for (unsigned short b = 0; b < presentMap.width; b++)
+			for (unsigned short b = 0; b < currMap.width; b++)
 			{
-				if (presentMap.getSite ({b, a}).owner == myID)
+				if (currMap.getSite ({b, a}).owner == myID)
 				{
 					float force_x = 0.0, force_y = 0.0;
 					for (std::set<hlt::Location>::iterator it = boarders.begin (); it != boarders.end (); it ++)
 					{
-						float  dist = presentMap.getDistance ({b, a}, *it);
-					//	log << "dist is " << dist << std::endl;
-						float angle = presentMap.getAngle    ({b, a}, *it);
-					//	float force = /*(float) presentMap.getSite (*it, STILL).production*/ 1.0 /*/ (float) presentMap.getSite (*it, STILL).production*/ / (dist * dist);
-					//	float force = /*(float) presentMap.getSite (*it, STILL).production*/ 1.0 / (float) presentMap.getSite (*it, STILL).production / (dist * dist);
-						float force = (float) presentMap.getSite (*it, STILL).production /* (float) presentMap.getSite (*it, STILL).production*/ / (dist * dist);
+						float  dist = currMap.getDistance ({b, a}, *it);
+						float angle = currMap.getAngle    ({b, a}, *it);
+						float force = compute_force (currMap.getSite (*it, STILL), dist, myArea);
 						force_x += force * cos (angle);
 						force_y += force * sin (angle);
 					}
-				//	log << force_x << ' ' << force_y << std::endl;
-					if (presentMap.getSite ({b, a}, STILL).strength < 16) continue;
 					unsigned char direction = STILL;
 					if (force_x + force_y > 0 && force_x - force_y > 0) direction = EAST;
 					if (force_x + force_y > 0 && force_x - force_y < 0) direction = SOUTH;
 					if (force_x + force_y < 0 && force_x - force_y < 0) direction = WEST;
 					if (force_x + force_y < 0 && force_x - force_y > 0) direction = NORTH;
-					if (presentMap.getSite ({b, a}, direction).owner != myID && presentMap.getSite ({b, a}, direction).strength >= presentMap.getSite ({b, a}).strength) continue;
-
-					moves.insert ({{b, a}, direction});
+					hlt::Site currSite = currMap.getSite ({b, a}, STILL);
+					hlt::Site nextSite = currMap.getSite ({b, a}, direction);
+					if (myArea <= 16 && currSite.strength <= 8 || myArea > 16 && currSite.strength <= 16) continue; // let small pieces be resting ans growing
+					if (nextSite.owner != myID && currSite.strength <= nextSite.strength)
+					{
+							 if (clock < 8 && currMap.getSite ({b, a}, (direction + 0) & 0x3 + 1).owner == myID) fout << "combined!" << std::endl;
+							 if (currMap.getSite ({b, a}, (direction + 0) & 0x3 + 1).owner == myID) direction = (direction + 0) & 0x3 + 1;
+						else if (currMap.getSite ({b, a}, (direction + 2) & 0x3 + 1).owner == myID) direction = (direction + 2) & 0x3 + 1;
+						else continue; // avoid death move
+					}
+					moveList.insert ({{b, a}, direction});
 				}
 			}
 		}
 
-        sendFrame(moves);
-    }
+		sendFrame (moveList);
+	}
 
-    return 0;
+	return 0;
 }
+
